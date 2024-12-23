db87548ff552e46aa16833856d402d6bdc8fad89
Sakshi Pokhriyal <sakshi.pokhriyal007@gmail.com>
2024-12-23 21:18:39 +0530

    Add files via upload

---
+++ 95.cpp
@@ -0,0 +1,31 @@
+//demonstrate template with single parameter with class:
+#include <iostream>
+using namespace std;
+template <class T1>
+class Test
+{
+    T1 a;
+
+public:
+    Test(T1 x)
+    {
+        a = x;
+    }
+    void show()
+    {
+        cout << a << "\n";
+    }
+};
+int main()
+{
+    Test<float> test1(1.23F);
+    Test<int> test2(100);
+    Test<string> test3("Sakshi");
+    Test<char> test4('S');
+    test1.show();
+    test2.show();
+    test3.show();
+    test4.show();
+
+    return 0;
+}
